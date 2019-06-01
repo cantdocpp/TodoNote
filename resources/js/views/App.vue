510aeda05085802be14ea01a95613247fde275fd
cantdocpp <cihuyman95@gmail.com>
2019-06-01 18:04:47 +0700

    adding vue js for the spa

---
+++ resources/js/views/App.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="main">
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <router-link :to="{ name: 'home' }" class="brand">TodoNote</router-link>
+            </div>
+        </nav>
+
+        <div class="container">
+            <div class="main__content">
+                <div class="sidenav">
+                    <aside class="menu">
+                        <ul class="menu-list">
+                            <li><router-link :to="{ name: 'home' }">Today</router-link></li>
+                            <li><router-link :to="{ name: 'home' }">Next 7 Days</router-link></li>
+                        </ul>
+                    </aside>
+                </div>
+                <div class="content">
+                    <router-view></router-view>
+                </div>
+            </div>
+        </div>  
+    </div>
+    
+
+</template>
+<script>
+    export default {}
+</script>
+
+<style lang="scss" scoped>
+    a {
+        color: black;
+    }
+
+    .brand {
+        position: absolute;
+        margin-top: 8px;
+        display: inline-block;
+        font-size: 20px;
+    }
+
+    nav {
+        padding: 0 10px;
+        height: 50px !important;
+        position: relative;
+    }
+
+    .container {
+        position: relative;
+        padding: 10px;
+    }
+
+    .sidenav {
+        width: 25%;
+    }
+
+    aside {
+        overflow-y: auto;
+    }
+
+    .main__content {
+        display: flex;
+        padding-top: 20px;
+    }
+
+
+    @media only screen and (max-width: 768px) {
+        .navbar-brand {
+            display: none;
+        }
+
+        aside {
+            display: none;
+        }
+    }
+</style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TodoNote</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+  </head>
+  <body>
+      <div id="app">
+          <app></app>
+      </div>
+
+      <script src="{{ mix('js/app.js') }}"></script>
+  </body>
+</html>
